49be8132c18957ec0cc38ebb65368a9de0964720
Duarte Lucas <duarteprista@gmail.com>
2017-07-04 14:28:24 +0100

    profile policy

--- better_than_AirBnB/app/controllers/application_controller.rb
+++ better_than_AirBnB/app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: protected
+  alias_method :current_user, :current_member
+  # Pundit uses user, we changed it to member
+
+  rescue_from Pundit::NotAuthorizedError, with: :member_not_authorized
+  private
+
+  def member_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
