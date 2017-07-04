20f8df7f466340aceec97afa382676549d64f9f6
Duarte Lucas <duarteprista@gmail.com>
2017-07-04 14:35:20 +0100

    profile authorized in every profiles_controller method

--- better_than_AirBnB/app/controllers/profiles_controller.rb
+++ better_than_AirBnB/app/controllers/profiles_controller.rb
@@ -1,31 +1,35 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
-  before_action :set_member
+  before_action :set_member, :authenticate_member!
   # GET /profiles
   # GET /profiles.json
   def index
+    authorize@profile #pundit method
     @profiles = Profile.all
   end
 
   # GET /profiles/1
   # GET /profiles/1.json
   def show
+    authorize@profile
   end
 
   # GET /profiles/new
   def new
     @profile = @member.build_profile
+    authorize@profile #here it has not been set before action
   end
 
   # GET /profiles/1/edit
   def edit
+    authorize@profile
   end
 
   # POST /profiles
   # POST /profiles.json
   def create
     @profile = @member.create_profile(profile_params)
-
+    authorize@profile #again, after profile has been declared
     respond_to do |format|
       if @profile.save
         format.html { redirect_to member_profile_path(@member, @profile), notice: 'Profile was successfully created.' }
@@ -40,6 +44,7 @@ class ProfilesController < ApplicationController
   # PATCH/PUT /profiles/1
   # PATCH/PUT /profiles/1.json
   def update
+    authorize@profile
     respond_to do |format|
       if @profile.update(profile_params)
         format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
@@ -54,6 +59,7 @@ class ProfilesController < ApplicationController
   # DELETE /profiles/1
   # DELETE /profiles/1.json
   def destroy
+    authorize@profile
     @profile.destroy
     respond_to do |format|
       format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
